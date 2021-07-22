c9c91ba4c88c492640fc7e3d16f353292d9ea83b
jgzeni <zenister@gmail.com>
2021-07-22 18:03:57 -0300

    Adding database guards for unique and the README file

--- apis/models/equipment.py
+++ apis/models/equipment.py
@@ -7,7 +7,7 @@ class equipment(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     vessel_id = db.Column(db.BigInteger, db.ForeignKey('vessels.id'))
     name = db.Column(db.String(256))
-    code = db.Column(db.String(8))
+    code = db.Column(db.String(8), unique=True)
     location = db.Column(db.String(256))
     active = db.Column(db.Boolean)
 
--- apis/models/vessel.py
+++ apis/models/vessel.py
@@ -5,5 +5,5 @@ class vessel(db.Model):
     __tablename__ = 'vessels'
 
     id = db.Column(db.BigInteger, primary_key=True)
-    code = db.Column(db.String(8))
+    code = db.Column(db.String(8), unique=True)
 
